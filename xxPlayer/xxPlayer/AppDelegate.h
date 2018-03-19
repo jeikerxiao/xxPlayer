ef1dcd3c1328869e853727933499c9dc39b1c719
jeikerxiao <jeiker@126.com>
2018-03-19 14:39:44 +0800

    init project
    init project

---
+++ xxPlayer/xxPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xxPlayer
+//
+//  Created by xiao on 2018/3/19.
+//  Copyright © 2018年 jeikerxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
